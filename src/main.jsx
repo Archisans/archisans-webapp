ff1f8cb81d7c90ad10da386c4da6af8382e889ea
psabhishek26 <sploitpay.aosp.test@gmail.com>
2025-10-21 21:07:10 +0530

    Include vercel analytics
    Signed-off-by: psabhishek26 <sploitpay.aosp.test@gmail.com>

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { RootProviders } from "./RootProviders";
+import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <RootProviders>
     <App />
+    <Analytics />
   </RootProviders>
 );
